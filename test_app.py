b7c201fd03a82dbefc95129a44bde8c3ada4d252
jai613 <110447410+jai613@users.noreply.github.com>
2024-11-20 02:05:03 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -9,7 +9,7 @@ def test_is_prime():
     assert is_prime(5) == True
     assert is_prime(7) == True
     assert is_prime(11) == True
-    assert is_prime(33) == True
+    assert is_prime(3) == True
     
     # Test non-prime numbers
     assert is_prime(1) == False
